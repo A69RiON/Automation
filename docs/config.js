65acbf6bd858c5ae021fb05a97fd6ff0e827be8b
A69RiON <177922276+A69RiON@users.noreply.github.com>
2026-08-12 08:26:50 +1000

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -1,5 +1,5 @@
 window.DEAL_HUNTER_CONFIG = {
-  SUPABASE_URL: "",
-  SUPABASE_PUBLISHABLE_KEY: "",
+  SUPABASE_URL: "https://jtslgfldtbrojqvaoogm.supabase.co",
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_mtLgMUq9pO3GYKpuMaEIsQ_9LqIPGwe",
   ADMIN_EMAIL_SUFFIX: "@au-deal-hunter.local"
 };
